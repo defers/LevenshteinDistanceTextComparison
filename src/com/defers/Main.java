bb4a796ccd26961adedc46f6c24a02df6bf1f2af
Алексей <defers86@gmail.com>
2023-03-10 00:19:50 +0300

    10032023

--- src/com/defers/Main.java
+++ src/com/defers/Main.java
@@ -2,8 +2,6 @@ package com.defers;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 public class Main {
 
@@ -41,57 +39,69 @@ class StringComparator {
         int resultWeigh = 0;
 
         for (int idx = 0; idx < tokens1.length; idx++) {
+
             String token1 = tokens1[idx];
             Map<String, Double> gradationMap = new HashMap<>();
 
             for (int jdx = 0; jdx < tokens2.length; jdx++) {
+
                 String token2 = tokens2[jdx];
 
                 int len1 = token1.length();
                 int len2 = token2.length();
-
                 int[][] matrix = new int[len1+1][len2+1];
-                fillFirstRowAndColumn(matrix, len1, len2);
 
-                int cost;
-                int distance = 0;
-                for (int i = 1; i <= len1; i++) {
-                    for (int j = 1; j <= len2; j++) {
-                        cost = token1.charAt(i-1) == token2.charAt(j-1) ? 0 : 1;
-                        int[] previousRow = matrix[i-1];
-                        int[] currentRow = matrix[i];
-                        matrix[i][j] = Math.min(Math.min(previousRow[j] + 1, currentRow[j-1] + 1), previousRow[j-1] + cost);
-                        distance = matrix[i][j];
-                    }
-                }
+                fillFirstRowAndColumn(matrix, len1, len2);
+                int distance = calculateLevenshteinDistance(matrix, token1, token2, len1, len2);
                 double percentDiff = distance == 0 ? 0 : Math.ceil(distance / (double) len1 * 100);
                 gradationMap.put(token2, percentDiff);
-                if (percentDiff == 0) {
-                    resultWeigh += 10;
-                } else if (percentDiff <= 10) {
-                    resultWeigh += 5;
-                } else if (percentDiff <= 30) {
-                    resultWeigh += 3;
-                } else if (percentDiff <= 50) {
-                    resultWeigh += 1;
-                }
+                resultWeigh += specifyPercentage(percentDiff);
             }
-
             resultGradation.put(token1, gradationMap);
         }
+        return resultWeigh;
+    }
 
+    private int calculateLevenshteinDistance(int[][] matrix, String token1,
+                                             String token2, int len1, int len2) {
+        int cost;
+        int distance = 0;
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                cost = token1.charAt(i-1) == token2.charAt(j-1) ? 0 : 1;
+                int[] previousRow = matrix[i-1];
+                int[] currentRow = matrix[i];
+                matrix[i][j] = Math.min(Math.min(previousRow[j] + 1, currentRow[j-1] + 1), previousRow[j-1] + cost);
+                distance = matrix[i][j];
+            }
+        }
+        return distance;
+    }
+
+    private int specifyPercentage(double percentDiff) {
+        int resultWeigh = 0;
+
+        if (percentDiff == 0) {
+            resultWeigh = 10;
+        } else if (percentDiff <= 10) {
+            resultWeigh = 5;
+        } else if (percentDiff <= 30) {
+            resultWeigh = 3;
+        } else if (percentDiff <= 50) {
+            resultWeigh = 1;
+        }
 
         return resultWeigh;
     }
 
     private void fillFirstRowAndColumn(int[][] matrix, int len1, int len2) {
-        matrix[0][0] = 0;
+        //matrix[0][0] = 0;
 
-        for (int i = 1; i <= len1; i++) {
+        for (int i = 0; i <= len1; i++) {
             matrix[i][0] = i;
         }
 
-        for (int i = 1; i <= len2; i++) {
+        for (int i = 0; i <= len2; i++) {
             matrix[0][i] = i;
         }
     }
